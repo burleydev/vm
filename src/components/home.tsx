20d1bf7787c6568dbbf7548006e24f7cae042aee
Carlos Borrega <c.borrega@proton.me>
2025-03-10 12:22:10 +0000

    changed brown colour

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,12 +5,12 @@ const Home = () => {
     <div className='h-screen flex flex-col text-center justify-evenly bg-brown'>
       
       <div className='mt-[10%] mb-10'>
-        <h1 className='font-bold tracking-wider lg:text-8xl md:text-7xl sm:text-6xl xs:text-4xl'>PORTFOLIO</h1>
-        <h2 className='font-bold mt-10 tracking-wider lg:text-4xl md:text-3xl sm:text-2xl xs:text-md'>VISUAL MERCHANDISER</h2>
+        <h1 className='font-bold tracking-wider lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl'>PORTFOLIO</h1>
+        <h2 className='font-bold mt-10 tracking-wider lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl'>VISUAL MERCHANDISER</h2>
       </div>
 
       <div className='mb-10'>
-        <h3 className='font-bold sm:text-2xl xs:text-sm'>CARLOS BORREGA</h3>
+        <h3 className='font-bold lg:text-2xl md:text-xl sm:text-lg xs:text-md'>CARLOS BORREGA</h3>
       </div>
     </div>
   );
