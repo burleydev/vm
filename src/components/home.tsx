9676bfe28d6215ac10370dd369571283b35bd045
Carlos Borrega <c.borrega@proton.me>
2025-03-10 11:29:11 +0000

    fixed main section

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,9 +2,9 @@ import '../index.css';
 
 const Home = () => {
   return (
-    <div className='h-screen flex flex-col text-center justify-between bg-brown'>
+    <div className='h-screen flex flex-col text-center justify-evenly bg-brown'>
       
-      <div className='mt-[22%] mb-10'>
+      <div className='mt-[10%] mb-10'>
         <h1 className='font-bold tracking-wider lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl'>PORTFOLIO</h1>
         <h2 className='font-bold mt-4 tracking-wider lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl'>VISUAL MERCHANDISER</h2>
       </div>
