1be2c386ace32ffa048fc589e65c501a6fbd39f4
Carlos Borrega <c.borrega@proton.me>
2025-03-12 18:27:37 +0000

    updated home image

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,12 +5,12 @@ const Home = () => {
   return (
     <div className='h-screen flex flex-col text-center justify-evenly bg-cover bg-top' style={{backgroundImage: `url(${home})`}}>
       
-      <div className='mt-[10%] mb-10'>
+      <div className='lg:mt-[12%] md:mt-[20%] sm:mt-[30%] xs:mt-[40%]'>
         <h1 className='title font-bold tracking-wider lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl'>PORTFOLIO</h1>
         <h2 className='title font-bold mt-10 tracking-wider lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl'>VISUAL MERCHANDISING</h2>
       </div>
 
-      <div className='mb-10'>
+      <div className=''>
         <h3 className='title font-bold lg:text-2xl md:text-xl sm:text-lg xs:text-md'>CARLOS BORREGA</h3>
       </div>
     </div>
