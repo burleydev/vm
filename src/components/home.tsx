f87a26da75717615cd27a27e0e1e800964bb339d
Carlos Borrega <c.borrega@proton.me>
2025-03-12 17:04:47 +0000

    updated home image

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,16 +1,17 @@
 import '../index.css';
+// import home from '../images/weekday/home.jpg';
 
 const Home = () => {
   return (
-    <div className='h-screen flex flex-col text-center justify-evenly bg-brown'>
+    <div className='h-screen flex flex-col text-center justify-evenly background'>
       
       <div className='mt-[10%] mb-10'>
-        <h1 className='font-bold tracking-wider lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl'>PORTFOLIO</h1>
-        <h2 className='font-bold mt-10 tracking-wider lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl'>VISUAL MERCHANDISING</h2>
+        <h1 className='title font-bold tracking-wider lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl'>PORTFOLIO</h1>
+        <h2 className='title font-bold mt-10 tracking-wider lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl'>VISUAL MERCHANDISING</h2>
       </div>
 
       <div className='mb-10'>
-        <h3 className='font-bold lg:text-2xl md:text-xl sm:text-lg xs:text-md'>CARLOS BORREGA</h3>
+        <h3 className='title font-bold lg:text-2xl md:text-xl sm:text-lg xs:text-md'>CARLOS BORREGA</h3>
       </div>
     </div>
   );
