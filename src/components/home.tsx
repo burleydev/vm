27d707d48c21914c087b23b0a1590b06053f89cf
Carlos Borrega <c.borrega@proton.me>
2025-03-14 11:41:26 +0000

    updated h-screen in home

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -3,7 +3,7 @@ import home from '../images/weekday/home.jpg';
 
 const Home = () => {
   return (
-    <div className='h-screen flex flex-col text-center justify-evenly bg-cover bg-top' style={{backgroundImage: `url(${home})`}}>
+    <div className='h-screen min-h-[40rem] flex flex-col text-center justify-evenly bg-cover bg-top' style={{backgroundImage: `url(${home})`}}>
       
       <div className='lg:mt-[12%] md:mt-[20%] sm:mt-[30%] xs:mt-[40%]'>
         <h1 className='title font-bold tracking-wider lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl'>PORTFOLIO</h1>
