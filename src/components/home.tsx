28863d80af346d6735479c23732fe5963df3a965
Carlos Borrega <c.borrega@proton.me>
2025-09-05 11:54:31 +0100

    updated info

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -6,12 +6,8 @@ const Home = () => {
     <div className='h-screen min-h-[40rem] flex flex-col text-center justify-evenly bg-cover bg-top' style={{backgroundImage: `url(${home})`}}>
       
       <div className='lg:mt-[12%] md:mt-[20%] sm:mt-[30%] xs:mt-[40%]'>
-        <h1 className='title font-bold tracking-wider lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl'>PORTFOLIO</h1>
-        <h2 className='title font-bold mt-10 tracking-wider lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl'>VISUAL MERCHANDISING</h2>
-      </div>
-
-      <div className=''>
-        <h3 className='title font-bold tracking-wider lg:text-2xl md:text-xl sm:text-lg xs:text-md'>CARLOS BORREGA</h3>
+        <h1 className='title1 white lg:text-8xl md:text-7xl sm:text-6xl xs:text-5xl'>CARLOS BORREGA</h1>
+        <h2 className='title2 white mt-10 lg:text-xl md:text-lg sm:text-md xs:text-sm'>VISUAL MERCHANDISER PORTFOLIO</h2>
       </div>
     </div>
   );
